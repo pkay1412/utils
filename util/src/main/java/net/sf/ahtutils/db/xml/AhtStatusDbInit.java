88691502fc882f1e1ee5df30bd88be81819da6b3
thorsten-k <t.kisner@web.de>
2011-05-24 11:04:43 +0000

    public methods removed

--- util/src/main/java/net/sf/ahtutils/db/xml/AhtStatusDbInit.java
+++ util/src/main/java/net/sf/ahtutils/db/xml/AhtStatusDbInit.java
@@ -64,7 +64,7 @@ public class AhtStatusDbInit
 	public UtilsStatus removeData(UtilsStatus ejbStatus)
 	{
 		Map<String,UtilsLang> dbLangMap = ejbStatus.getName();
-		ejbStatus.setName(null);		
+		ejbStatus.setName(null);
 		for(UtilsLang lang : dbLangMap.values()){sDeleteLangs.add(lang.getId());}
 		return ejbStatus;
 	}
@@ -107,7 +107,7 @@ public class AhtStatusDbInit
 	public <S extends UtilsStatus<L>,L extends UtilsLang> void deleteUnusedStatus(AhtUtilsStatusInterface fStatus, Class<S> cStatus, Class<L> cLang)
 	{
 		logger.debug("Deleing unused Status/Langs");
-		for(long id : getsDeleteLangs())
+		for(long id : sDeleteLangs)
 		{
 			try
 			{
@@ -119,10 +119,9 @@ public class AhtStatusDbInit
 			catch (AhtUtilsNotFoundException e) {logger.error(e);}
 		}
 		
-		 Map<String, Set<Long>> mapUnusedStatus = getmDbAvailableStatus();
-		 for(String group : mapUnusedStatus.keySet())
+		 for(String group : mDbAvailableStatus.keySet())
 		 {
-			 Set<Long> sIds = mapUnusedStatus.get(group);
+			 Set<Long> sIds = mDbAvailableStatus.get(group);
 			 logger.trace("Deleting Group "+group+": "+sIds.size());
 			 for(long id : sIds)
 			 {
@@ -138,10 +137,6 @@ public class AhtStatusDbInit
 		 }
 	}
 	
-	public void setStatusEjbFactory(UtilsStatusEjbFactory statusEjbFactory) {this.statusEjbFactory = statusEjbFactory;}
-	public Set<Long> getsDeleteLangs() {return sDeleteLangs;}
-	public Map<String, Set<Long>> getmDbAvailableStatus() {return mDbAvailableStatus;}
-	
 	public <S extends UtilsStatus<L>,L extends UtilsLang> void iuStatus(List<Status> list, AhtUtilsStatusInterface fStatus, Class<S> cStatus, Class<L> cLang)
 	{
 		for(Status status : list)
@@ -155,13 +150,16 @@ public class AhtStatusDbInit
 					List<UtilsStatus> l = new ArrayList<UtilsStatus>();
 					for(S s : fStatus.allAhtUtilsStatus(cStatus)){l.add(s);}
 					savePreviousDbEntries(status.getGroup(), l);
+					logger.trace("Delete Pool: "+mDbAvailableStatus.get(status.getGroup()).size());
 				}
 				try
 				{
 					ejbStatus = fStatus.fAhtUtilsStatusByCode(cStatus,status.getCode());
 					ejbStatus = (S)removeData(ejbStatus);
 					ejbStatus = fStatus.updateAhtUtilsStatus(ejbStatus);
+					ejbStatus = fStatus.fAhtUtilsEntity(cStatus, ejbStatus.getId());
 					removeStatusFromDelete(status.getGroup(), ejbStatus.getId());
+					logger.trace("Now in Pool: "+mDbAvailableStatus.get(status.getGroup()).size());
 					logger.trace("Found: "+ejbStatus);
 				}
 				catch (AhtUtilsNotFoundException e)
@@ -171,8 +169,14 @@ public class AhtStatusDbInit
 					ejbStatus = fStatus.persistAhtUtilsStatus(ejbStatus);
 					logger.trace("Added: "+ejbStatus);
 				}
-				logger.trace("Updating Langs ... "+ejbStatus);
-				try {addLangs(ejbStatus,status);}
+				
+				try
+				{
+//					logger.trace("Updating Info ... "+ejbStatus);
+//					logger.debug(ejbStatus.getName().get("de").getId());
+					addLangs(ejbStatus,status);
+//					logger.debug(ejbStatus.getName().get("de").getId());
+				}
 				catch (InstantiationException e) {logger.error(e);}
 				catch (IllegalAccessException e) {logger.error(e);}
 				catch (AhtUtilsIntegrityException e) {logger.error(e);}
@@ -183,4 +187,6 @@ public class AhtStatusDbInit
 			catch (IllegalAccessException e) {logger.error(e);}
 		}
 	}
+	
+	public void setStatusEjbFactory(UtilsStatusEjbFactory statusEjbFactory) {this.statusEjbFactory = statusEjbFactory;}
 }
