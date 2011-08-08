d2bd1dd5d02161d61d8b4131eca6de6ea10b3f81
hhemmer <hhemmer@web.de>
2011-08-08 16:17:21 +0000

    Added logic from Maven report pre-compiler goal to ant task.

--- util/src/main/java/net/sf/ahtutils/report/AntJasperCreatorTask.java
+++ util/src/main/java/net/sf/ahtutils/report/AntJasperCreatorTask.java
@@ -1,5 +1,18 @@
 package net.sf.ahtutils.report;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import net.sf.ahtutils.xml.report.Jr;
+import net.sf.ahtutils.xml.report.Media;
+import net.sf.ahtutils.xml.report.Report;
+import net.sf.ahtutils.xml.report.Reports;
+import net.sf.exlp.util.xml.JaxbUtil;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.ConfigurationFactory;
@@ -7,44 +20,66 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
+import org.jdom.JDOMException;
 
 public class AntJasperCreatorTask extends Task{
 	
-	private Boolean pdf, xls, rtl;
-	
-
-	private String srcDir, dstDir;
-	
-	private String configFile;
+	private String configFile, reportRoot;
 
     static Log logger = LogFactory.getLog(AntJasperCreatorTask.class);
-	protected Configuration config;
 	
     public void execute() throws BuildException
     {
-    	//Create Configuration object from configuration file
-        ConfigurationFactory factory = new ConfigurationFactory(configFile);
-        Configuration config = null;
-        try {
-			config = factory.getConfiguration();
-		} catch (ConfigurationException e) {
-			e.printStackTrace();
-		}
-		
-		//Get the ReportController from ahtutils
-		ReportController reportController = new ReportController(config);
-
-		//Get a list of all reports in configuration file
-		String xPathPrefix = "//report";
-		int reportCount = (config.getStringArray(xPathPrefix)).length;
-		logger.debug("Pre-Compiling "+reportCount+" Reports");
-		for(int i=1;i<=reportCount;i++)
-		{
-			//Compile reports and save to file
-			String rId = config.getString(xPathPrefix+"/["+i+"]/@id");
-			reportController.setParameter("en", rId);
-		}
-    }
+    	 logger.info("Using " +configFile +" for report configuration.");
+ 		
+ 		Reports reports;
+ 		try {reports = (Reports)JaxbUtil.loadJAXB(reportRoot +"/" +configFile, Reports.class);}
+ 		catch (FileNotFoundException e) {logger.error(e.getMessage());}
+ 		
+ 		//Compiling reports
+ 		logger.info("Pre-Compiling "+reports.getReport().size()+" Report(s)");
+ 		for(Report report : reports.getReport())
+ 		{
+ 			logger.info("Report: "+report.getId() +" (" +report.getMedia().size() + " media outputs)");
+ 			for (Media media : report.getMedia())
+ 			{
+ 				logger.info("Compiling " +media.getJr().size() +" reports for output -" +media.getType() +"-");
+ 				for (Jr jr : media.getJr())
+ 				{
+ 					logger.info(jr.getName());
+ 					//Compiling for left to right and right to left languages
+ 					
+ 					//Compiling ltr version
+ 					String jrxml  = reportRoot +"/" +"jrxml"  +"/" +report.getDir() +"/" + media.getType() + "/" + jr.getType() + jr.getName() +".jrxml";
+ 					String jasperLtr = reportRoot +"/" +"jasper" +"/" +report.getDir() +"/" + media.getType() + "/ltr/" + jr.getType() + jr.getName() +".jasper";
+ 					logger.info("Compiling " +jrxml +" to " +jasperLtr);
+ 					try {
+ 						new File(reportRoot +"/" +"jasper"  +"/" +report.getDir() +"/" + media.getType() + "/ltr/").mkdirs();
+ 						JasperCompileManager.compileReportToFile(jrxml, jasperLtr);
+ 					} catch (JRException e) {
+ 						logger.error(e.getMessage());
+ 					}
+ 					
+ 					//Compiling rtl version
+ 					String jasperRtl = reportRoot +"/" +"jasper" +"/" +report.getDir() +"/" + media.getType() + "/rtl/" + jr.getType() + jr.getName() +".jasper";
+ 					logger.info("Compiling " +jrxml +" to " +jasperRtl);
+ 					new File(reportRoot +"/" +"jasper"  +"/" +report.getDir() +"/" + media.getType() + "/rtl/").mkdirs();
+ 					InputStream in = null;
+ 					try {
+ 						in = ReportUtil.LeftToRightConversion(jrxml);
+ 					} catch (JDOMException e) {
+ 						logger.error("Problem converting to right-to-left language.");
+ 					}
+ 					try {
+ 						JasperCompileManager.compileReportToStream(in, new FileOutputStream(jasperRtl));
+ 					} catch (FileNotFoundException e) {
+ 						logger.error(e.getMessage());
+ 					} catch (JRException e) {
+ 						logger.error(e.getMessage());
+ 					}
+ 				}
+ 			}
+ 		}
 
 	public static void main(String[] args) {
 		AntJasperCreatorTask task = new AntJasperCreatorTask();
@@ -61,52 +96,12 @@ public class AntJasperCreatorTask extends Task{
 		this.configFile = configFile;
 	}
 
-	public Configuration getConfig() {
-		return config;
+
+	public String getReportRoot() {
+		return reportRoot;
 	}
 
-	public void setConfig(Configuration config) {
-		this.config = config;
+	public void setReportRoot(String reportRoot) {
+		this.reportRoot = reportRoot;
 	}
-	
-	public Boolean getPdf() {
-		return pdf;
-	}
-
-	public void setPdf(Boolean pdf) {
-		this.pdf = pdf;
-	}
-
-	public Boolean getXls() {
-		return xls;
-	}
-
-	public void setXls(Boolean xls) {
-		this.xls = xls;
-	}
-
-	public Boolean getRtl() {
-		return rtl;
-	}
-
-	public void setRtl(Boolean rtl) {
-		this.rtl = rtl;
-	}
-
-	public String getSrcDir() {
-		return srcDir;
-	}
-
-	public void setSrcDir(String srcDir) {
-		this.srcDir = srcDir;
-	}
-
-	public String getDstDir() {
-		return dstDir;
-	}
-
-	public void setDstDir(String dstDir) {
-		this.dstDir = dstDir;
-	}
-
 }
