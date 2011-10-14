c7fd9e6df345802225f2de3b6c7887af6f7d8c00
thorsten-k <t.kisner@web.de>
2011-10-14 09:33:46 +0000

    Test for RankComparator added

--- util/src/main/java/net/sf/ahtutils/controller/util/Rank.java
+++ util/src/main/java/net/sf/ahtutils/controller/util/Rank.java
@@ -1,37 +1,33 @@
 package net.sf.ahtutils.controller.util;
 
-public class Rank {
-public int index, points;
-	
-public	Rank ()
+import java.util.Comparator;
+
+public class Rank implements Comparator<Rank>
+{
+	public int index, points;
+
+	public	Rank ()
 	{
 		
 	}
+	public	Rank (int index, int points)
+	{
+		this.index = index;
+		this.points = points;
+	}
 
-public	Rank (int index, int points)
-{
-	this.index = index;
-	this.points = points;
-	
-}
+	public int getIndex() {return index;}
+	public void setIndex(int index) {this.index = index;}
 
-public int getIndex() {
-	return index;
-}
+	public int getPoints() {return points;}
+	public void setPoints(int points) {this.points = points;}
 
-public void setIndex(int index) {
-	this.index = index;
-}
-
-public int getPoints() {
-	return points;
-}
-
-public void setPoints(int points) {
-	this.points = points;
-}	
-	
-
+	@Override
+	public int compare(Rank a, Rank b)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}	
 
 }
 
---
+++ util/src/test/java/net/sf/ahtutils/test/controller/util/TestRankComparator.java
@@ -0,0 +1,39 @@
+package net.sf.ahtutils.test.controller.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import junit.framework.Assert;
+import net.sf.ahtutils.controller.util.Rank;
+import net.sf.ahtutils.test.AbstractAhtUtilTest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class TestRankComparator extends AbstractAhtUtilTest
+{
+	static Log logger = LogFactory.getLog(TestRankComparator.class);
+	
+	@Before
+	public void init()
+	{
+		
+	}
+ 
+    @Test
+    public void testSimple()
+    {	
+    	List<Rank> list = new ArrayList<Rank>();
+    	Rank a = new Rank(1,4);list.add(a);
+    	Rank b = new Rank(2,5);list.add(b);
+    	
+    	Collections.sort(list, new Rank());
+    	Assert.assertEquals(b, list.get(0));
+    	Assert.assertEquals(a, list.get(1));
+    }
+}
