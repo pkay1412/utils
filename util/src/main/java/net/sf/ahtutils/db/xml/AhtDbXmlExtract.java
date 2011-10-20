5c657e5fe7fc08992f39e814ed0ededa09b9133c
thorsten-k <t.kisner@web.de>
2011-10-20 15:46:46 +0000

    AhtUtilsConfigurationException now thrown

--- util/src/main/java/net/sf/ahtutils/db/xml/AhtDbXmlExtract.java
+++ util/src/main/java/net/sf/ahtutils/db/xml/AhtDbXmlExtract.java
@@ -2,8 +2,10 @@ package net.sf.ahtutils.db.xml;
 
 import java.io.FileNotFoundException;
 
+import net.sf.ahtutils.controller.exception.AhtUtilsConfigurationException;
+
 public interface AhtDbXmlExtract
 {
-	void extract() throws FileNotFoundException; 
-	void ideUpdate();
+	void extract() throws FileNotFoundException,AhtUtilsConfigurationException; 
+	void ideUpdate() throws AhtUtilsConfigurationException;
 }
