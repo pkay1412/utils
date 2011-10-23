77c8a393d6f8289f33e7acea0be6e9b9863f00be
thorsten-k <t.kisner@web.de>
2011-10-23 08:18:39 +0000

    Test for ofx:chart rendering added

---
+++ util/src/test/java/net/sf/ahtutils/test/chart/TestOfxChartRenderer.java
@@ -0,0 +1,102 @@
+package net.sf.ahtutils.test.chart;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Random;
+
+import net.sf.exlp.util.DateUtil;
+import net.sf.exlp.util.io.LoggerInit;
+import net.sf.exlp.util.xml.JaxbUtil;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.junit.Test;
+import org.openfuxml.addon.chart.OFxChartRenderControl;
+import org.openfuxml.addon.chart.data.jaxb.Chart;
+import org.openfuxml.addon.chart.data.jaxb.Charttype;
+import org.openfuxml.addon.chart.data.jaxb.Container;
+import org.openfuxml.addon.chart.data.jaxb.Data;
+import org.openfuxml.xml.ns.OfxNsPrefixMapper;
+
+public class TestOfxChartRenderer
+{
+	static Log logger = LogFactory.getLog(TestOfxChartRenderer.class);
+	
+	public TestOfxChartRenderer()
+	{
+		
+	}
+	
+	public Chart getTimeSeries()
+	{
+		Chart chart = new Chart();
+		chart.setLegend(true);
+		
+		chart.setCharttype(getType());
+		chart.setGrid(getGrid());
+		
+		chart.getContainer().add(getX("a"));
+		return chart;
+	}
+	
+	private Chart.Grid getGrid()
+	{
+		Chart.Grid grid = new Chart.Grid();
+		grid.setDomain(false);
+		grid.setRange(false);
+		return grid;
+	}
+	
+	private Charttype getType()
+	{
+		Charttype type = new Charttype();
+		Charttype.Timebar tBar = new Charttype.Timebar();
+		tBar.setShadow(false);
+		tBar.setGradient(false);
+		type.setTimebar(tBar);
+		return type;
+	}
+	
+	private Container getX(String label)
+	{
+		Random rnd = new Random();
+		Container x = new Container();
+		x.setLabel(label);
+		for(int i=1;i<5;i++)
+		{
+			Data data = new Data();
+			data.setRecord(DateUtil.getXmlGc4D(DateUtil.getDateFromInt(2010, 1, i)));
+			data.setY(1+rnd.nextInt(i));
+			if(rnd.nextInt(100)<70){x.getData().add(data);}
+		}
+		return x;
+	}
+	
+	@Test
+	public void renderDirect() throws IOException
+    {	
+		TestOfxChartRenderer test = new TestOfxChartRenderer();
+		Chart chart = test.getTimeSeries();
+		JaxbUtil.debug2(this.getClass(),chart, new OfxNsPrefixMapper());
+			
+		OFxChartRenderControl ofxRenderer = new OFxChartRenderControl();
+		JFreeChart jfreeChart = ofxRenderer.render(chart);
+		
+		OutputStream os = new FileOutputStream(new File("target/chart.png"));
+		ChartUtilities.writeChartAsPNG(os,jfreeChart,800,300);
+    }
+	
+	public static void main (String[] args) throws Exception
+	{
+		LoggerInit loggerInit = new LoggerInit("log4j.xml");	
+			loggerInit.addAltPath("config");
+			loggerInit.init();
+		
+		TestOfxChartRenderer test = new TestOfxChartRenderer();
+		test.renderDirect();
+	}
+}
