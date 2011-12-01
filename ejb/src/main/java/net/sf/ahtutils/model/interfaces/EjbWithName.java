e5f93cd6867f66a99f7a798287be88e0e188130c
thorsten-k <t.kisner@web.de>
2011-12-01 09:44:33 +0000

    empty folders removed

--- ejb/src/main/java/net/sf/ahtutils/model/interfaces/EjbWithName.java
+++
@@ -1,7 +0,0 @@
-package net.sf.ahtutils.model.interfaces;
-
-public interface EjbWithName
-{	
-	public String getName();
-	public void setName(String name);
-}
--- ejb/src/main/java/net/sf/ahtutils/model/interfaces/EjbWithValidFrom.java
+++
@@ -1,9 +0,0 @@
-package net.sf.ahtutils.model.interfaces;
-
-import java.util.Date;
-
-public interface EjbWithValidFrom
-{
-	public Date getValidFrom();
-	public void setValidFrom(Date validFrom);
-}
