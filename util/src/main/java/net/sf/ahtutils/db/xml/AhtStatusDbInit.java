9d4cfa1863d6715961187b12b5b949297c2c23f4
thorsten-k <t.kisner@web.de>
2011-12-04 09:28:36 +0000

    warnings removed

--- util/src/main/java/net/sf/ahtutils/db/xml/AhtStatusDbInit.java
+++ util/src/main/java/net/sf/ahtutils/db/xml/AhtStatusDbInit.java
@@ -14,7 +14,6 @@ import net.sf.ahtutils.controller.exception.AhtUtilsIntegrityException;
 import net.sf.ahtutils.controller.exception.AhtUtilsNotFoundException;
 import net.sf.ahtutils.controller.factory.ejb.status.EjbStatusFactory;
 import net.sf.ahtutils.controller.interfaces.AhtUtilsFacade;
-import net.sf.ahtutils.model.interfaces.status.UtilsDescription;
 import net.sf.ahtutils.model.interfaces.status.UtilsLang;
 import net.sf.ahtutils.model.interfaces.status.UtilsStatus;
 import net.sf.ahtutils.xml.aht.Aht;
