4f79c0c1134763eab276e9322089e4cdf0dc2157
thorsten-k <t.kisner@web.de>
2011-12-06 14:56:20 +0000

    XML ACL category added (UTILS-65)

---
+++ xml/src/test/java/net/sf/ahtutils/xml/access/TestXmlCategory.java
@@ -0,0 +1,65 @@
+package net.sf.ahtutils.xml.access;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import net.sf.ahtutils.xml.status.TestXmlDescriptions;
+import net.sf.ahtutils.xml.status.TestXmlLangs;
+import net.sf.exlp.util.io.LoggerInit;
+import net.sf.exlp.util.xml.JaxbUtil;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestXmlCategory extends AbstractXmlAccessTest
+{
+	final static Logger logger = LoggerFactory.getLogger(TestXmlCategory.class);
+	
+	@BeforeClass
+	public static void initFiles()
+	{
+		fXml = new File(rootDir,"category.xml");
+	}
+    
+    @Test
+    public void testXml() throws FileNotFoundException
+    {
+    	Category actual = create();
+    	Category expected = JaxbUtil.loadJAXB(fXml.getAbsolutePath(), Category.class);
+    	assertJaxbEquals(expected, actual);
+    }
+    
+    private static Category create(){return create(true);}
+    public static Category create(boolean withChilds)
+    {
+    	Category xml = new Category();
+    	xml.setCode("myCode");
+    	xml.setIndex(1);
+    	
+    	if(withChilds)
+    	{
+    		xml.setLangs(TestXmlLangs.create(false));
+    		xml.setDescriptions(TestXmlDescriptions.create(false));
+    		xml.setProjectRoles(TestXmlProjectRoles.create(false));
+    		xml.setRoles(TestXmlRoles.create(false));
+    		xml.setUsecases(TestXmlUsecases.create(false));
+    	}
+    	
+    	return xml;
+    }
+    
+    public void save() {save(create(),fXml);}
+	
+	public static void main(String[] args)
+    {
+		LoggerInit loggerInit = new LoggerInit("log4j.xml");	
+			loggerInit.addAltPath("src/test/resources/config");
+			loggerInit.init();		
+			
+		TestXmlCategory.initFiles();	
+		TestXmlCategory test = new TestXmlCategory();
+		test.save();
+    }
+}
