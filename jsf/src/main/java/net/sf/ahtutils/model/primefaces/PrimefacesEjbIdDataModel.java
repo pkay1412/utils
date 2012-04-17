ab429ea7e6cd14f03e949a80fde98f71b4238859
thorsten-k <t.kisner@web.de>
2012-04-17 08:37:37 +0000

    PrimefacesEjbIdDataModel added

---
+++ jsf/src/main/java/net/sf/ahtutils/model/primefaces/PrimefacesEjbIdDataModel.java
@@ -0,0 +1,60 @@
+package net.sf.ahtutils.model.primefaces;
+
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.model.ListDataModel;
+
+import net.sf.ahtutils.model.interfaces.EjbWithId;
+
+import org.primefaces.model.SelectableDataModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class  PrimefacesEjbIdDataModel <T extends EjbWithId> extends ListDataModel<T> implements SelectableDataModel<T>
+{    
+	final static Logger logger = LoggerFactory.getLogger(PrimefacesEjbIdDataModel.class);
+	
+	private Map<Long,Boolean> mapUnlock;
+  
+    public Map<Long, Boolean> getMapUnlock(){return mapUnlock;}
+
+	public PrimefacesEjbIdDataModel(List<T> data)
+    {  
+        super(data);
+        mapUnlock = new Hashtable<Long,Boolean>();
+    }  
+      
+    @SuppressWarnings("unchecked")
+    @Override  
+    public T getRowData(String rowKey)
+    {  
+    	List<T> list = (List<T>) getWrappedData();  
+         
+        for(T ejb : list)
+        {  
+            if(ejb.getId()==(new Integer(rowKey)))
+            {
+            	return ejb;
+            }  
+        }
+          
+        return null;  
+    }  
+  
+    @Override  
+    public Object getRowKey(T item) {return item.getId();}
+    
+    public void unlock(T item) {mapUnlock.put(item.getId(), true);}
+    
+    @SuppressWarnings("unchecked")
+    public void update(T item)
+    {
+		List<T> list = (List<T>) getWrappedData();
+		int index = list.indexOf(item);
+		list.set(index, item);
+		this.setWrappedData(list);
+		this.setRowIndex(index);
+    }
+}
--- jsf/src/main/java/net/sf/ahtutils/web/mbean/util/AbstractIconBean.java
+++ jsf/src/main/java/net/sf/ahtutils/web/mbean/util/AbstractIconBean.java
@@ -16,15 +16,19 @@ public class AbstractIconBean implements Serializable
 	private static final long serialVersionUID = 1L;
 	
 	private UtilsFacade fUtils;
+	private String imagePath;
 	
 	private Map<Long,String> mapImages;
+	protected Map<String,String> mapStatic;
 	
 	//******* Methods *******************************
 
-    public void initFacade(UtilsFacade fUtils)
+    public void initFacade(UtilsFacade fUtils, String imagePath)
     {
 		this.fUtils=fUtils;
+		this.imagePath=imagePath;
 		mapImages = new Hashtable<Long,String>();
+		mapStatic = new Hashtable<String,String>();
     }
 
     @SuppressWarnings("rawtypes")
@@ -38,9 +42,20 @@ public class AbstractIconBean implements Serializable
     private void generate(int size,  UtilsStatus status)
     {
     	StringBuffer sb = new StringBuffer();
-    	sb.append("/images/").append(size).append("/");
+    	sb.append("/").append(imagePath);
+    	sb.append("/").append(size);
+    	sb.append("/");
     	if(status.getImage()!=null){sb.append(status.getImage());}
     	else{sb.append("noImage.png");}
     	mapImages.put(status.getId(), sb.toString());
     }
+    
+	public String icon(int size,  String key)
+	{
+		StringBuffer sb = new StringBuffer();
+    	sb.append("/").append(imagePath);
+    	sb.append("/").append(size);
+    	sb.append("/").append(mapStatic.get(key));
+		return sb.toString();
+	}
 }
