56e7567cf9d2c66d0d43ef7b4a97fc9de499b4d8
thorsten-k <t.kisner@web.de>
2012-04-25 08:29:46 +0000

    XML link added (UTILS-119)

---
+++ xml/src/test/java/net/sf/ahtutils/xml/mail/TestXmlLink.java
@@ -0,0 +1,55 @@
+package net.sf.ahtutils.xml.mail;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import net.sf.ahtutils.test.UtilsXmlTstBootstrap;
+import net.sf.exlp.util.xml.JaxbUtil;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestXmlLink extends AbstractXmlMailTest
+{
+	final static Logger logger = LoggerFactory.getLogger(TestXmlLink.class);
+	
+	@BeforeClass
+	public static void initFiles()
+	{
+		fXml = new File(rootDir,"link.xml");
+	}
+    
+    @Test
+    public void xml() throws FileNotFoundException
+    {
+    	Link actual = createLink(true);
+    	Link expected = JaxbUtil.loadJAXB(fXml.getAbsolutePath(), Link.class);
+    	assertJaxbEquals(expected, actual);
+    }  
+    
+    public static Link createLink(boolean withChilds)
+    {
+    	Link xml = new Link();
+
+    	xml.setId(123);
+    	xml.setRefId(345);
+    	xml.setType("myType");
+    	xml.setToken("sndjgkdjngkjngkjnweg");
+    	    	
+    	return xml;
+    }
+    
+    public void save() {save(createLink(true),fXml);}
+	
+	public static void main(String[] args)
+    {
+		UtilsXmlTstBootstrap.init();
+			
+		TestXmlLink.initPrefixMapper();
+		TestXmlLink.initFiles();	
+		TestXmlLink test = new TestXmlLink();
+		test.save();
+    }
+}
