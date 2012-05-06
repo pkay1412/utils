ebd12575c7b01a3b19cd6c22215282820ab9942f
hhemmer <hhemmer@web.de>
2012-05-06 13:45:28 +0000

    Re-created report module (UTILS-111)

--- report/src/main/java/net/sf/ahtutils/report/ReportUtilTemplate.java
+++ report/src/main/java/net/sf/ahtutils/report/ReportUtilTemplate.java
@@ -3,9 +3,12 @@ package net.sf.ahtutils.report;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import net.sf.ahtutils.xml.report.Element;
 import net.sf.ahtutils.xml.report.Field;
+import net.sf.ahtutils.xml.report.Jr;
 import net.sf.ahtutils.xml.report.Media;
 import net.sf.ahtutils.xml.report.Report;
+import net.sf.ahtutils.xml.report.Reports;
 import net.sf.ahtutils.xml.report.Template;
 import net.sf.exlp.util.xml.JaxbUtil;
 import net.sf.jasperreports.engine.JRBand;
@@ -15,6 +18,7 @@ import net.sf.jasperreports.engine.design.JRDesignExpression;
 import net.sf.jasperreports.engine.design.JRDesignField;
 import net.sf.jasperreports.engine.design.JRDesignTextField;
 import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.engine.xml.JRXmlWriter;
 
 import org.slf4j.Logger;
@@ -24,11 +28,18 @@ public class ReportUtilTemplate
 {
 	final static Logger logger = LoggerFactory.getLogger(ReportUtilXls.class);
 	
+	public ReportUtilTemplate(String reportRoot) throws FileNotFoundException{
+		Reports reports = JaxbUtil.loadJAXB(reportRoot +"reports.xml", Reports.class);
+	}
+	
 	public void applyTemplates(Report report)
 	{
 		for (Media media : report.getMedia())
 		{
-			
+			for (Jr jr : media.getJr())
+			{
+				
+			}
 		}
 	}
 	
@@ -36,6 +47,20 @@ public class ReportUtilTemplate
 	{
 		//JasperDesign  the in-memory JRXML report design
 		JasperDesign design = new JasperDesign();
+		
+		//Load the elements from template
+		for (Element element : template.getElement())
+		{
+			JasperDesign elementDesign = (JasperDesign) JRLoader.loadObject(element.getFile());
+			if (element.getType().equals("header"))
+			{
+				design.setPageHeader(elementDesign.getPageHeader());
+			}
+			if (element.getType().equals("footer"))
+			{
+				design.setPageFooter(elementDesign.getPageFooter());
+			}
+		}
 		
 		//Now, the standard fields need to be declared (holding title, subtitle, logo, footer and record date)
 		for (Field field : template.getField())
@@ -47,40 +72,13 @@ public class ReportUtilTemplate
 			design.addField(reportField);
 		}
 		
-		//A JRDesignBand can represent different report parts like the DetailBand, the PageHeader/Footer, ...
-		JRBand header = new JRDesignBand();
-		
-		//Example of a dynamic text field
-		JRDesignTextField textField=new JRDesignTextField();
-		textField.setStretchWithOverflow(true);
-		textField.setBlankWhenNull(true);
-		textField.setX(0);
-		textField.setY(4);
-		textField.setWidth(100);
-		textField.setHeight(13);
-		textField.setKey("header");
-	
-		//TextField expression
-		JRDesignExpression designExpression=new JRDesignExpression();
-		designExpression.setValueClass(String.class);
-		designExpression.setText("\"Welcome to the report.\"");
-
-		//Setting the expression
-		textField.setExpression(designExpression);
-		
-		//Add element to header band
-		header.getChildren().add(textField);
-		
-		//A band is then set as a part of the report design
-		design.setPageHeader(header);
-		
 		//JRXmlWriter cares about writing the in-memory design to an OutputStream
 		JRXmlWriter.writeReport(design, System.out, "UTF-8");
 	}
 	
 	public static void main(String[] args) throws JRException, FileNotFoundException
 	{
-		ReportUtilTemplate templater = new ReportUtilTemplate();
+		ReportUtilTemplate templater = new ReportUtilTemplate(null);
 		String rootDir = "../xml/src/test/resources/data/xml/report";
 		
 		File fXml = new File(rootDir,"template.xml");
