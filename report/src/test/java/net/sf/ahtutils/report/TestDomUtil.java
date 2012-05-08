570b68f7de8cbca5b5c7d12bb64ee5f83d35f186
thorsten-k <t.kisner@web.de>
2012-05-08 06:18:46 +0000

    example for DomUtil added (UTILS-32)

---
+++ report/src/test/java/net/sf/ahtutils/report/TestDomUtil.java
@@ -0,0 +1,48 @@
+package net.sf.ahtutils.report;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import net.sf.ahtutils.test.AbstractAhtUtilTest;
+import net.sf.ahtutils.xml.report.Info;
+import net.sf.ahtutils.xml.report.Info.Title;
+import net.sf.exlp.util.xml.DomUtil;
+import net.sf.exlp.util.xml.JaxbUtil;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class TestDomUtil extends AbstractAhtUtilTest
+{
+	final static Logger logger = LoggerFactory.getLogger(TestDomUtil.class);
+    	
+	private Element root;
+	private Info expected;
+	
+	@Before
+	public void init() throws IOException, ParserConfigurationException
+	{
+		Title title = new Title();
+		title.setValue("MyTitle");
+		
+		expected = new Info();
+		expected.setTitle(title);
+		
+		Document doc = JaxbUtil.toW3CDocument(expected);
+		root = doc.getDocumentElement();
+		
+	}
+	
+	@Test
+	public void domUtil()
+	{
+		Info actual = DomUtil.toJaxb(root, Info.class);
+		this.assertJaxbEquals(expected, actual);
+	}
+	
+}
---
+++ report/src/test/java/net/sf/ahtutils/test/AbstractAhtUtilTest.java
@@ -0,0 +1,51 @@
+package net.sf.ahtutils.test;
+
+import java.io.File;
+
+import net.sf.exlp.util.io.LoggerInit;
+import net.sf.exlp.util.xml.JaxbUtil;
+import net.sf.exlp.xml.ns.NsPrefixMapperInterface;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbstractAhtUtilTest
+{
+	final static Logger logger = LoggerFactory.getLogger(AbstractAhtUtilTest.class);
+	
+	protected static NsPrefixMapperInterface nsPrefixMapper;
+	protected File f;
+	protected boolean saveReference=false;
+
+	protected static File fTarget;
+	
+	protected static void setfTarget(File myTarget) {fTarget=myTarget;}
+
+	@BeforeClass
+	public static void initFile()
+	{
+		if(!LoggerInit.isLog4jInited()){initLogger();}
+		String dirTarget = System.getProperty("targetDir");
+		if(dirTarget==null){dirTarget="target";}
+		setfTarget(new File(dirTarget));
+		logger.debug("Using targeDir "+fTarget.getAbsolutePath());
+	}
+	
+	@BeforeClass
+    public static void initLogger()
+	{
+		if(!LoggerInit.isLog4jInited())
+		{
+			LoggerInit loggerInit = new LoggerInit("log4junit.xml");	
+			loggerInit.addAltPath("config.ahtutils-report.test");
+			loggerInit.init();
+		}
+    }
+	
+	protected void assertJaxbEquals(Object expected, Object actual)
+	{
+		Assert.assertEquals("actual XML differes from expected XML",JaxbUtil.toString(expected),JaxbUtil.toString(actual));
+	}
+}
