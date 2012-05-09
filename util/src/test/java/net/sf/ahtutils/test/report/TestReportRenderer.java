71c160cffc1dc1bd95c30fd638452d4752552179
hhemmer <hhemmer@web.de>
2012-05-09 14:31:31 +0000

    Migrated TestReportRenderer to new JDOM based report handling (UTILS-125)

--- util/src/test/java/net/sf/ahtutils/test/report/TestReportRenderer.java
+++ util/src/test/java/net/sf/ahtutils/test/report/TestReportRenderer.java
@@ -1,8 +1,12 @@
 package net.sf.ahtutils.test.report;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Locale;
 
+import net.sf.ahtutils.report.ReportHandler;
 import net.sf.ahtutils.report.AbstractReportControl.Direction;
 import net.sf.ahtutils.report.AbstractReportControl.Output;
 import net.sf.ahtutils.report.exception.ReportException;
@@ -48,9 +52,15 @@ public class TestReportRenderer extends AbstractAhtUtilReportTest
 		AhtUtilTstBootstrap.init();
 		
 		TestReportRenderer.initReport();
+		ReportHandler reportHandler = new ReportHandler("src/main/resources/reports.ahtutils-util/reports.xml");
+		
 		TestReportRenderer test = new TestReportRenderer();
-		test.initExample();
-		test.initController();
-		test.pdfOutput();
+		test.initExample("testReport");
+		
+		ByteArrayOutputStream pdf = reportHandler.createUsingJDom(reportId, test.jdomReport, ReportHandler.Format.pdf, Locale.GERMAN);
+		String pdfFile = "target/" +reportId +".pdf";
+		OutputStream outputStream = new FileOutputStream (pdfFile);
+		pdf.writeTo(outputStream);
+		logger.info("Writing PDF stream to file: "+pdfFile);
     }
 }
