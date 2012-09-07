ca104e477a686b77a10cb9777912579039be682b
thorsten-k <t.kisner@web.de>
2012-09-07 05:00:20 +0000

    JSF component and actionListener implemented (UTILS-144)

---
+++ jsf/src/main/java/net/sf/ahtutils/jsf/components/PfSaveUpdate.java
@@ -0,0 +1,17 @@
+package net.sf.ahtutils.jsf.components;
+
+import javax.el.MethodExpression;
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UINamingContainer;
+import javax.faces.context.FacesContext;
+
+@FacesComponent(value="net.sf.ahtutils.jsf.components.PfSaveUpdate")
+public class PfSaveUpdate extends UINamingContainer
+{
+    public void actionListener()
+    {
+        FacesContext context = FacesContext.getCurrentInstance();
+        MethodExpression ajaxEventListener = (MethodExpression) getAttributes().get("actionListener");
+        ajaxEventListener.invoke(context.getELContext(), new Object[] {});
+    }
+}
