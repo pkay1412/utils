ceef4c681cb887c007b5aea971df91f7571e3788
thorsten-k <t.kisner@web.de>
2012-09-10 10:25:08 +0000

    FacesContextMessage added

---
+++ jsf/src/main/java/net/sf/ahtutils/jsf/util/FacesContextMessage.java
@@ -0,0 +1,28 @@
+package net.sf.ahtutils.jsf.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class FacesContextMessage
+{
+	
+	public static void info(String summary, String detail)
+	{
+		FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_INFO,summary,detail);
+		addMessage(fm);
+	}
+	
+	public static void error(String summary, String detail)
+	{
+		addMessage(new FacesMessage(FacesMessage.SEVERITY_ERROR,summary,detail));
+	}
+	
+	public static void addMessage(FacesMessage message)
+	{  
+		addMessage(null, message);  
+	}
+	public static void addMessage(String id, FacesMessage message)
+	{  
+		FacesContext.getCurrentInstance().addMessage(id, message);  
+	}
+}
