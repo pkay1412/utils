d66b61d9b2cad093056df098522c00b7e15a003c
thorsten-k <t.kisner@web.de>
2012-09-26 09:39:27 +0000

    UtilsValueCurrency added

--- interfaces/src/main/java/net/sf/ahtutils/model/interfaces/finance/UtilsValueCurrency.java
+++ interfaces/src/main/java/net/sf/ahtutils/model/interfaces/finance/UtilsValueCurrency.java
@@ -2,7 +2,7 @@ package net.sf.ahtutils.model.interfaces.finance;
 
 import net.sf.ahtutils.model.interfaces.status.UtilsLang;
 
-public interface UtilsValueCurrency<L extends UtilsLang, C extends UtilsCurrency<L>> extends UtilsFinance
+public interface UtilsValueCurrency<C extends UtilsCurrency<L>, L extends UtilsLang> extends UtilsFinance
 {
 	C getCurrency();
 	void setCurrency(C currency);
