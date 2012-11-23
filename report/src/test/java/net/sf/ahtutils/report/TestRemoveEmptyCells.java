8200c8e5a3a4c7529bada5688533e9d79f87efc2
thorsten-k <t.kisner@web.de>
2012-11-23 19:24:31 +0000

    dependencies optimized

--- report/src/test/java/net/sf/ahtutils/report/TestRemoveEmptyCells.java
+++ report/src/test/java/net/sf/ahtutils/report/TestRemoveEmptyCells.java
@@ -1,4 +1,4 @@
-package net.sf.ahtutils.test.report;
+package net.sf.ahtutils.report;
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
@@ -6,13 +6,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import net.sf.ahtutils.report.ReportUtilXls;
-import net.sf.ahtutils.test.AbstractAhtUtilTest;
+import net.sf.ahtutils.test.AbstractAhtUtilsReportTest;
 
 import org.junit.Ignore;
 
 @Ignore("Needs to be implemented: UTILS-74")
-public class TestRemoveEmptyCells extends AbstractAhtUtilTest{
+public class TestRemoveEmptyCells extends AbstractAhtUtilsReportTest
+{
 	
 	public void convert(){
 		OutputStream os = ReportUtilXls.RemoveEmptyCells("src/test/resources/reports/excelPoiTest.xls");
--- report/src/test/java/net/sf/ahtutils/test/AbstractAhtUtilsReportTest.java
+++ report/src/test/java/net/sf/ahtutils/test/AbstractAhtUtilsReportTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class AbstractAhtUtilsReportTest extends AbstractAhtUtilsReportTst
 {
 	final static Logger logger = LoggerFactory.getLogger(AbstractAhtUtilsReportTest.class);
