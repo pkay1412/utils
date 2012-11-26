907c02ae405d9399d9e0588fff66572fa097d1b1
thorsten-k <t.kisner@web.de>
2012-11-26 05:49:48 +0000

    versions dependency management moved to bom (UTILS-166)

--- web/src/main/java/net/sf/ahtutils/web/test/Test166Bean.java
+++ web/src/main/java/net/sf/ahtutils/web/test/Test166Bean.java
@@ -1,12 +1,19 @@
 package net.sf.ahtutils.web.test;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
+import net.sf.ahtutils.controller.factory.ejb.status.EjbStatusFactory;
 import net.sf.ahtutils.exception.ejb.UtilsIntegrityException;
+import net.sf.ahtutils.model.ejb.status.AhtUtilsDescription;
+import net.sf.ahtutils.model.ejb.status.AhtUtilsLang;
+import net.sf.ahtutils.model.ejb.status.AhtUtilsStatus;
+import net.sf.ahtutils.model.primefaces.PrimefacesEjbIdDataModel;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +23,7 @@ public class Test166Bean
 {
 	final static Logger logger = LoggerFactory.getLogger(Test166Bean.class);
 	
-//	private PrimefacesEjbIdDataModel<AhtUtilsStatus> dmStatus;
+	private PrimefacesEjbIdDataModel<AhtUtilsStatus> dmStatus;
 
 	private String test ="hello";
 	
@@ -28,7 +35,7 @@ public class Test166Bean
 	public void init() throws FileNotFoundException, InstantiationException, IllegalAccessException, UtilsIntegrityException
 	{
 		logger.info("@PostConstruct");
-/*		
+		
 		EjbStatusFactory<AhtUtilsStatus,AhtUtilsLang,AhtUtilsDescription> f = EjbStatusFactory.createFactory(AhtUtilsStatus.class, AhtUtilsLang.class, AhtUtilsDescription.class);
 		
 		List<AhtUtilsStatus> list = new ArrayList<AhtUtilsStatus>();
@@ -39,8 +46,8 @@ public class Test166Bean
 		list.add(s1);
 		list.add(s2);
 		dmStatus = new PrimefacesEjbIdDataModel<AhtUtilsStatus>(list);
-*/	}
+	}
 	
-//	public PrimefacesEjbIdDataModel<AhtUtilsStatus> getDmStatus() {return dmStatus;}
+	public PrimefacesEjbIdDataModel<AhtUtilsStatus> getDmStatus() {return dmStatus;}
 	
 }
