eb1b840333551b87d8d071a4a9d92d861c7c06b7
thorsten-k <t.kisner@web.de>
2013-01-04 14:11:49 +0000

    highlight component with between attribute

--- jsf/src/main/java/net/sf/ahtutils/jsf/components/Highlight.java
+++ jsf/src/main/java/net/sf/ahtutils/jsf/components/Highlight.java
@@ -17,7 +17,57 @@ public class Highlight extends UINamingContainer
 {
 	final static Logger logger = LoggerFactory.getLogger(Highlight.class);
 	
-	public String text(String input, int from, int to)
+	private static final String id ="//ahtutils.highlight:"; 
+	
+	public String between(String input, String between, boolean relativeTab)
+	{
+		InputStream is = this.getClass().getResourceAsStream(input);
+		
+		Integer from = null;
+		Integer to = null;
+		List<String> list;
+		
+		if(is!=null)
+		{
+			try
+			{
+				list = IOUtils.readLines(is, "UTF-8");
+				for(int i=0;i<list.size();i++)
+				{
+					String s = list.get(i).trim();
+					if(s.startsWith(id))
+					{
+						String[] token = s.split(":");
+						if(token[1].equals(between))
+						{
+							if(from==null){from=i+2;}
+							else{to=i;}
+						}
+					}
+				}
+			}
+			catch (IOException e) {return e.getMessage();}
+			
+		}
+		else {return resourceNotFound(input);}
+		
+		if(from==null || to==null)
+		{
+			StringBuffer sb = new StringBuffer();
+			sb.append("Not all markers");
+			sb.append(" '").append(between).append("'");
+			sb.append(" found in: ").append(input).append(IOUtils.LINE_SEPARATOR_UNIX);
+			sb.append("from: ").append(from).append(IOUtils.LINE_SEPARATOR_UNIX);
+			sb.append("to: ").append(to);
+			return sb.toString();
+		}
+		else
+		{
+			return build(list,from,to);
+		}
+	}
+	
+	public String text(String input, int from, int to, boolean relativeTab)
 	{
 		logger.trace("Looking for Resource "+input);
 		logger.trace("from:"+from+" "+"to:"+to);
@@ -32,14 +82,7 @@ public class Highlight extends UINamingContainer
 				try
 				{
 					List<String> list = IOUtils.readLines(is, "UTF-8");
-					for(int i=0;i<list.size();i++)
-					{
-						if(i>=from && i<=to)
-						{
-							String s = list.get(i);
-							writer.write(s+IOUtils.LINE_SEPARATOR_UNIX);
-						}
-					}
+					writer.write(build(list, from, to));
 				}
 				catch (IOException e){logger.error(e.getMessage());e.printStackTrace();}
 			}
@@ -52,23 +95,29 @@ public class Highlight extends UINamingContainer
 			try {writer.close();} catch (IOException e) {e.printStackTrace();}
 			return s;
 		}
-		else
-		{
-			StringBuffer sb = new StringBuffer();
-			sb.append("Resource not found: ").append(IOUtils.LINE_SEPARATOR_UNIX);
-			sb.append(input).append(IOUtils.LINE_SEPARATOR_UNIX);
-			sb.append(" must be available in the classpath");
-			return sb.toString();
-		}
+		else {return resourceNotFound(input);}
 	}
 	
-	public String tab(int tab)
+	private String build(List<String> list, int from, int to)
+	{
+		StringWriter writer = new StringWriter();
+		for(int i=1;i<=list.size();i++)
+		{
+			if(i>=from && i<=to)
+			{
+				String s = list.get(i-1);
+				writer.write(s+IOUtils.LINE_SEPARATOR_UNIX);
+			}
+		}
+		return writer.toString();
+	}
+	
+	private String resourceNotFound(String input)
 	{
 		StringBuffer sb = new StringBuffer();
-		for(int i=0;i<tab;i++)
-		{
-			sb.append(" ");
-		}
+		sb.append("Resource not found: ").append(IOUtils.LINE_SEPARATOR_UNIX);
+		sb.append(input).append(IOUtils.LINE_SEPARATOR_UNIX);
+		sb.append(" must be available in the classpath");
 		return sb.toString();
 	}
 }
