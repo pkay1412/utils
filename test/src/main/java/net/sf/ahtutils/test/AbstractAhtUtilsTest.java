b9df45975123575c8ad04f8a9ed39dc4079b4344
thorsten-k <t.kisner@web.de>
2013-06-22 12:35:20 +0000

    maven target directory now handled by AbstractAhtUtilsTest

--- test/src/main/java/net/sf/ahtutils/test/AbstractAhtUtilsTest.java
+++ test/src/main/java/net/sf/ahtutils/test/AbstractAhtUtilsTest.java
@@ -1,7 +1,15 @@
 package net.sf.ahtutils.test;
 
+import java.io.File;
+import java.util.Date;
 import java.util.Random;
 
+import net.sf.exlp.util.DateUtil;
+import net.sf.exlp.util.io.LoggerInit;
+import net.sf.exlp.util.xml.JaxbUtil;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,7 +17,29 @@ public class AbstractAhtUtilsTest
 {
 	final static Logger logger = LoggerFactory.getLogger(AbstractAhtUtilsTest.class);
 	
+	protected static File fTarget;
 	protected static Random rnd;
+	
+	@BeforeClass
+	public static void initTargetDirectory()
+	{
+		if(fTarget==null)
+		{
+			String dirTarget = System.getProperty("targetDir");
+			if(dirTarget==null){dirTarget="target";}
+			setfTarget(new File(dirTarget));
+			if(LoggerInit.isLog4jInited())
+			{
+				logger.debug("Using targeDir "+fTarget.getAbsolutePath());
+			}
+		}
+	}
+	
+	protected static void initLogger()
+	{
+		System.err.println("Must be overridden!!");
+		System.exit(-1);
+	}
 	
 	protected long rndL()
 	{
@@ -36,4 +66,16 @@ public class AbstractAhtUtilsTest
     	}
     	return sb.toString();
     }
+	
+	protected void assertJaxbEquals(Object ref, Object test)
+	{
+		Assert.assertEquals(JaxbUtil.toString(ref),JaxbUtil.toString(test));
+	}
+	
+	protected static Date getDefaultDate()
+	{
+		return DateUtil.getDateFromInt(2011, 11, 11, 11, 11, 11);
+	}
+	
+	protected static void setfTarget(File myTarget) {fTarget=myTarget;}
 }
