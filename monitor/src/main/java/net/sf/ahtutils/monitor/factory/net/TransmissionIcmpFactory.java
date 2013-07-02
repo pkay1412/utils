e9189acad2c736e21006adfdaeea1a614cd34947
thorsten-k <t.kisner@web.de>
2013-07-02 20:13:46 +0000

    prototype package create for icmp (UTILS-192)

---
+++ monitor/src/main/java/net/sf/ahtutils/monitor/factory/net/TransmissionIcmpFactory.java
@@ -0,0 +1,49 @@
+package net.sf.ahtutils.monitor.factory.net;
+
+import net.sf.ahtutils.monitor.factory.AbstractTransmissionFactory;
+import net.sf.ahtutils.monitor.result.net.DnsResult;
+import net.sf.ahtutils.monitor.result.net.IcmpResult;
+import net.sf.ahtutils.xml.monitoring.Data;
+import net.sf.ahtutils.xml.monitoring.DataSet;
+import net.sf.ahtutils.xml.monitoring.Indicator;
+import net.sf.exlp.util.DateUtil;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TransmissionIcmpFactory extends AbstractTransmissionFactory
+{
+	final static Logger logger = LoggerFactory.getLogger(TransmissionIcmpFactory.class);
+	
+	public TransmissionIcmpFactory()
+	{
+		
+	}
+	
+	public Indicator build(long resolution)
+	{
+		Indicator indicator = new Indicator();		
+		indicator.setCode(DnsResult.class.getCanonicalName());
+		indicator.getDataSet().add(buildDataSet(resolution));
+		return indicator;
+	}
+	
+	private DataSet buildDataSet(long resolution)
+	{
+		DataSet ds = new DataSet();
+		
+		for(IcmpResult icmp : ufb.all(IcmpResult.class))
+        {
+        	ds.getData().add(buildData(icmp));
+        }
+		
+		return ds;
+	}
+	
+	private Data buildData(IcmpResult icmp)
+	{
+		Data data = new Data();
+		data.setRecord(DateUtil.toXmlGc(icmp.getRecord()));
+		return data;
+	}
+}
---
+++ monitor/src/main/java/net/sf/ahtutils/monitor/processor/net/DnsResultProcessor.java
@@ -0,0 +1,45 @@
+package net.sf.ahtutils.monitor.processor.net;
+
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import javax.persistence.EntityManager;
+
+import net.sf.ahtutils.monitor.result.net.DnsResult;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DnsResultProcessor implements Runnable
+{
+	final static Logger logger = LoggerFactory.getLogger(DnsResultProcessor.class);
+
+	private EntityManager em;
+	private CompletionService<DnsResult> csDns;
+	
+    public DnsResultProcessor(EntityManager em, CompletionService<DnsResult> csDns)
+    {
+  	    this.em=em;
+  	    this.csDns=csDns;
+    }
+
+	@Override
+	public void run()
+	{
+		while(true)
+        {
+            try
+            {
+                Future<DnsResult> future = csDns.take();
+                DnsResult result = future.get();
+                
+                em.getTransaction().begin();
+                em.persist(result);
+                em.getTransaction().commit();
+            }
+            catch (InterruptedException e) {e.printStackTrace();}
+            catch (ExecutionException e) {e.printStackTrace();}
+        }
+	}
+}
