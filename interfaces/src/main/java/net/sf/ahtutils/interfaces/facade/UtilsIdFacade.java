103c621fb5db1e39fefa97bcba34637f7aa9dc60
thorsten-k <t.kisner@web.de>
2013-07-11 18:48:11 +0000

    sources.jar created for interfaces

--- interfaces/src/main/java/net/sf/ahtutils/interfaces/facade/UtilsIdFacade.java
+++ interfaces/src/main/java/net/sf/ahtutils/interfaces/facade/UtilsIdFacade.java
@@ -3,8 +3,10 @@ package net.sf.ahtutils.interfaces.facade;
 import net.sf.ahtutils.exception.ejb.UtilsNotFoundException;
 import net.sf.ahtutils.model.interfaces.with.EjbWithId;
 
+//ahtutils.highlight:interface
 public interface UtilsIdFacade
 {
 	<T extends Object> T find(Class<T> type, long id) throws UtilsNotFoundException;
 	<T extends EjbWithId> T find(Class<T> type, T t);
 }
+//ahtutils.highlight:interface
