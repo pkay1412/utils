134a71338997cb0ea253ffc6c87476727536bd72
thorsten-k <t.kisner@web.de>
2013-10-18 06:01:37 +0000

    Interface DbSqlImport added

---
+++ interfaces/src/main/java/net/sf/ahtutils/interfaces/db/UtilsDbSqlImport.java
@@ -0,0 +1,8 @@
+package net.sf.ahtutils.interfaces.db;
+
+import net.sf.ahtutils.exception.processing.UtilsConfigurationException;
+
+public interface UtilsDbSqlImport
+{
+	void importSql() throws UtilsConfigurationException;
+}
