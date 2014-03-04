14880b18aabc0197dd5c9cbc7cf9d98ea1081b2f
thorsten-k <t.kisner@web.de>
2014-03-04 16:45:35 +0000

    simple ProcessingTimeTracker added

--- util/src/main/java/net/sf/ahtutils/monitor/ProcessingTimeTracker.java
+++ util/src/main/java/net/sf/ahtutils/monitor/ProcessingTimeTracker.java
@@ -2,8 +2,29 @@ package net.sf.ahtutils.monitor;
 
 public class ProcessingTimeTracker
 {
+	private long start;
+	private long stop;
+	
 	public ProcessingTimeTracker()
 	{
 		
+	}
+	
+	public void start()
+	{
+		start = System.currentTimeMillis();
+	}
+	
+	public void stop()
+	{
+		stop = System.currentTimeMillis();
+	}
+	
+	public String toTotalTime()
+	{
+		StringBuffer sb = new StringBuffer();
+		sb.append(stop-start);
+		sb.append(" ms");
+		return sb.toString();
 	}
 }
