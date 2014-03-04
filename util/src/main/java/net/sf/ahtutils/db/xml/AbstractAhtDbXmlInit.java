b0512ba25e1db60955ab64ef760a22d63a0372fd
thorsten-k <t.kisner@web.de>
2014-03-04 08:31:33 +0000

    AbstractDbRestInit added

--- util/src/main/java/net/sf/ahtutils/db/xml/AbstractAhtDbXmlInit.java
+++ util/src/main/java/net/sf/ahtutils/db/xml/AbstractAhtDbXmlInit.java
@@ -1,18 +1,11 @@
 package net.sf.ahtutils.db.xml;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-
-import net.sf.ahtutils.exception.ejb.UtilsIntegrityException;
-import net.sf.ahtutils.exception.processing.UtilsConfigurationException;
-import net.sf.ahtutils.interfaces.db.UtilsDbXmlInit.Priority;
 import net.sf.ahtutils.xml.dbseed.Db;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class AbstractAhtDbXmlInit extends UtilsDbXmlSeedUtil
+public abstract class AbstractAhtDbXmlInit extends AbstractDbRestInit
 {
 	final static Logger logger = LoggerFactory.getLogger(AbstractAhtDbXmlInit.class);
 
@@ -21,34 +14,8 @@ public abstract class AbstractAhtDbXmlInit extends UtilsDbXmlSeedUtil
 	
 	public AbstractAhtDbXmlInit(Db dbSeed, DataSource datasource, AhtXmlInitIdMapper idMapper, AhtStatusDbInit asdi)
 	{
-		super(dbSeed, datasource);
+		super(dbSeed, datasource,null);
 		this.idMapper=idMapper;
 		this.asdi=asdi;
-	}
-	
-	public void initFromXml(Priority priority) throws FileNotFoundException,UtilsIntegrityException, UtilsConfigurationException
-	{
-		switch(priority)
-		{
-			case statics: initStatics();break;
-			case required: initRequired();break;
-			case mandatory: initMandatory();break;
-			case optional: initOptional();break;
-		}
-	}
-	
-	protected void initStatics() throws FileNotFoundException,UtilsIntegrityException,UtilsConfigurationException{}
-	protected void initRequired() throws FileNotFoundException,UtilsIntegrityException,UtilsConfigurationException{}
-	protected void initMandatory() throws FileNotFoundException,UtilsIntegrityException,UtilsConfigurationException{}
-	protected void initOptional() throws FileNotFoundException,UtilsIntegrityException,UtilsConfigurationException{}
-	
-	public static List<Priority> allPriorities()
-	{
-		List<Priority> list = new ArrayList<Priority>();
-		list.add(Priority.statics);
-		list.add(Priority.required);
-		list.add(Priority.mandatory);
-		list.add(Priority.optional);
-		return list;
 	}
 }
---
+++ util/src/main/java/net/sf/ahtutils/db/xml/AbstractDbRestInit.java
@@ -0,0 +1,53 @@
+package net.sf.ahtutils.db.xml;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.ahtutils.exception.ejb.UtilsIntegrityException;
+import net.sf.ahtutils.exception.processing.UtilsConfigurationException;
+import net.sf.ahtutils.interfaces.db.UtilsDbXmlInit.Priority;
+import net.sf.ahtutils.xml.dbseed.Db;
+
+import org.apache.commons.configuration.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractDbRestInit extends UtilsDbXmlSeedUtil
+{
+	final static Logger logger = LoggerFactory.getLogger(AbstractDbRestInit.class);
+	
+	protected Configuration config;
+	
+	public AbstractDbRestInit(Db dbSeed, DataSource datasource, Configuration config)
+	{
+		super(dbSeed, datasource);
+		this.config=config;
+	}
+	
+	public void initFromXml(Priority priority) throws FileNotFoundException,UtilsIntegrityException, UtilsConfigurationException
+	{
+		switch(priority)
+		{
+			case statics: initStatics();break;
+			case required: initRequired();break;
+			case mandatory: initMandatory();break;
+			case optional: initOptional();break;
+		}
+	}
+	
+	protected void initStatics() throws FileNotFoundException,UtilsIntegrityException,UtilsConfigurationException{}
+	protected void initRequired() throws FileNotFoundException,UtilsIntegrityException,UtilsConfigurationException{}
+	protected void initMandatory() throws FileNotFoundException,UtilsIntegrityException,UtilsConfigurationException{}
+	protected void initOptional() throws FileNotFoundException,UtilsIntegrityException,UtilsConfigurationException{}
+	
+	public static List<Priority> allPriorities()
+	{
+		List<Priority> list = new ArrayList<Priority>();
+		list.add(Priority.statics);
+		list.add(Priority.required);
+		list.add(Priority.mandatory);
+		list.add(Priority.optional);
+		return list;
+	}
+}
