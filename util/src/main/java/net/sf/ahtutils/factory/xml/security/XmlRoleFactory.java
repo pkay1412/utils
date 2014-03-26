187e821a30383190acb416aee1c8b425cd3b1090
thorsten-k <t.kisner@web.de>
2014-03-26 07:17:57 +0000

    XmlRole and XmlRolesFactory added

--- util/src/main/java/net/sf/ahtutils/factory/xml/security/XmlRoleFactory.java
+++ util/src/main/java/net/sf/ahtutils/factory/xml/security/XmlRoleFactory.java
@@ -1,19 +1,50 @@
 package net.sf.ahtutils.factory.xml.security;
 
+import net.sf.ahtutils.model.interfaces.idm.UtilsUser;
+import net.sf.ahtutils.model.interfaces.security.UtilsSecurityAction;
+import net.sf.ahtutils.model.interfaces.security.UtilsSecurityCategory;
+import net.sf.ahtutils.model.interfaces.security.UtilsSecurityRole;
+import net.sf.ahtutils.model.interfaces.security.UtilsSecurityUsecase;
+import net.sf.ahtutils.model.interfaces.security.UtilsSecurityView;
+import net.sf.ahtutils.model.interfaces.status.UtilsDescription;
+import net.sf.ahtutils.model.interfaces.status.UtilsLang;
 import net.sf.ahtutils.xml.security.Role;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class XmlRoleFactory
+public class XmlRoleFactory<L extends UtilsLang,
+	D extends UtilsDescription, 
+	C extends UtilsSecurityCategory<L,D,C,R,V,U,A,USER>,
+	R extends UtilsSecurityRole<L,D,C,R,V,U,A,USER>,
+	V extends UtilsSecurityView<L,D,C,R,V,U,A,USER>,
+	U extends UtilsSecurityUsecase<L,D,C,R,V,U,A,USER>,
+	A extends UtilsSecurityAction<L,D,C,R,V,U,A,USER>,
+	USER extends UtilsUser<L,D,C,R,V,U,A,USER>>
 {
 	final static Logger logger = LoggerFactory.getLogger(XmlRoleFactory.class);
 		
+	private Role q;
+	
+	public XmlRoleFactory(Role q)
+	{
+		this.q=q;
+	}
+	
 	public static Role create(String code, String label)
 	{
 		Role xml = new Role();
 		xml.setCode(code);
 		xml.setLabel(label);
+		return xml;
+	}
+	
+	public Role build(R role)
+	{
+		Role xml = new Role();
+		
+		if(q.isSetCode()){xml.setCode(role.getCode());}
+		
 		return xml;
 		
 	}
