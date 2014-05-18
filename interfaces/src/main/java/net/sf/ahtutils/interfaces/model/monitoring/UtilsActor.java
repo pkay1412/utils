763c85e572be2ad0b3d0a601ad81c9a424f79b40
thorsten-k <t.kisner@web.de>
2014-05-18 17:43:14 +0000

    Generic Interface for Actor added

---
+++ interfaces/src/main/java/net/sf/ahtutils/interfaces/model/monitoring/UtilsActor.java
@@ -0,0 +1,9 @@
+package net.sf.ahtutils.interfaces.model.monitoring;
+
+import net.sf.ahtutils.model.interfaces.with.EjbWithCode;
+import net.sf.ahtutils.model.interfaces.with.EjbWithId;
+
+public interface UtilsActor extends EjbWithId,EjbWithCode
+{
+
+}
