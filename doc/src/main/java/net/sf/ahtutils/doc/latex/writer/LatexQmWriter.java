5c2e35fdc401f42c977863533c247cb2f592df5d
thorsten-k <t.kisner@web.de>
2014-05-24 16:31:42 +0000

    QM Categories and category now created (UTILS-219)

--- doc/src/main/java/net/sf/ahtutils/doc/latex/writer/LatexQmWriter.java
+++ doc/src/main/java/net/sf/ahtutils/doc/latex/writer/LatexQmWriter.java
@@ -5,16 +5,22 @@ import java.io.IOException;
 import java.io.StringWriter;
 
 import net.sf.ahtutils.doc.UtilsDocumentation;
+import net.sf.ahtutils.doc.ofx.qa.OfxQaCategoriesSectionFactory;
 import net.sf.ahtutils.doc.ofx.qa.OfxQaCategorySectionFactory;
 import net.sf.ahtutils.doc.ofx.qa.OfxQaTeamTableFactory;
+import net.sf.ahtutils.xml.qa.Category;
 import net.sf.ahtutils.xml.qa.Qa;
 import net.sf.ahtutils.xml.status.Translations;
 import net.sf.exlp.util.io.StringIO;
 
 import org.apache.commons.configuration.Configuration;
+import org.openfuxml.content.ofx.Section;
 import org.openfuxml.content.table.Table;
 import org.openfuxml.exception.OfxAuthoringException;
+import org.openfuxml.media.cross.LatexCrossMediaManager;
+import org.openfuxml.renderer.latex.content.structure.LatexSectionRenderer;
 import org.openfuxml.renderer.latex.content.table.LatexTableRenderer;
+import org.openfuxml.renderer.latex.preamble.LatexPreamble;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,6 +35,8 @@ public class LatexQmWriter
 	private String[] langs;
 	private String[] headerKeys;
 	
+	private LatexCrossMediaManager cmm;
+	
 	public LatexQmWriter(Configuration config, Translations translations,String[] langs)
 	{
 		this.config=config;
@@ -39,7 +47,11 @@ public class LatexQmWriter
 		headerKeys = new String[2];
 		headerKeys[0] = "auTableQaRole";
 		headerKeys[1] = "auTableQaName";
+		
+		cmm = new LatexCrossMediaManager(new File(baseLatexDir),config.getString(LatexCrossMediaManager.keyOfxLatexImageDir));
 	}
+	
+	// *****************************************************************************
 	
 	public void writeQaTeam(Qa qa) throws OfxAuthoringException, IOException
 	{
@@ -58,6 +70,8 @@ public class LatexQmWriter
 		writeTable(table, f);
 	}
 	
+	// *****************************************************************************
+	
 	public void writeQaCategories(Qa qa) throws OfxAuthoringException, IOException
 	{
 		for(String lang : langs)
@@ -70,9 +84,29 @@ public class LatexQmWriter
 	{
 		File f = new File(baseLatexDir+"/"+lang+"/section/qa/categories.tex");
 		
+		OfxQaCategoriesSectionFactory fOfx = new OfxQaCategoriesSectionFactory(config,lang,translations);
+		Section section = fOfx.build(qa,lang+"/section/qa/category");
+		writeSection(section, f);
+	}
+	
+	// *****************************************************************************
+	
+	public void writeQaCategory(Category category) throws OfxAuthoringException, IOException
+	{
+		for(String lang : langs)
+		{
+			writeQaCategory(category, lang);
+		}
+	}
+	
+	public void writeQaCategory(Category category, String lang) throws OfxAuthoringException, IOException
+	{
+		String path = lang+"/section/qa/category";
+		File f = new File(baseLatexDir+"/"+path+"/"+category.getCode()+".tex");
+		
 		OfxQaCategorySectionFactory fOfx = new OfxQaCategorySectionFactory(config,lang,translations);
-		Section section = fOfx.build(qa, headerKeys);
-		writeTable(table, f);
+		Section section = fOfx.build(category);
+		writeSection(section, f);
 	}
 	
 	private void writeTable(Table table, File f) throws OfxAuthoringException, IOException
@@ -82,6 +116,16 @@ public class LatexQmWriter
 		
 		StringWriter sw = new StringWriter();
 		tableRenderer.write(sw);
+		StringIO.writeTxt(f, sw.toString());
+	}
+	
+	private void writeSection(Section section, File f) throws OfxAuthoringException, IOException
+	{
+		LatexSectionRenderer sectionRenderer = new LatexSectionRenderer(cmm,1,new LatexPreamble());
+		sectionRenderer.render(section);
+		
+		StringWriter sw = new StringWriter();
+		sectionRenderer.write(sw);
 		StringIO.writeTxt(f, sw.toString());
 	}
 }
--- doc/src/main/java/net/sf/ahtutils/doc/ofx/qa/OfxQaCategoriesSectionFactory.java
+++ doc/src/main/java/net/sf/ahtutils/doc/ofx/qa/OfxQaCategoriesSectionFactory.java
@@ -3,43 +3,49 @@ package net.sf.ahtutils.doc.ofx.qa;
 import net.sf.ahtutils.doc.DocumentationCommentBuilder;
 import net.sf.ahtutils.doc.ofx.AbstractUtilsOfxDocumentationFactory;
 import net.sf.ahtutils.xml.qa.Category;
+import net.sf.ahtutils.xml.qa.Qa;
 import net.sf.ahtutils.xml.status.Translations;
 
 import org.apache.commons.configuration.Configuration;
 import org.openfuxml.content.ofx.Comment;
-import org.openfuxml.content.ofx.Paragraph;
 import org.openfuxml.content.ofx.Section;
 import org.openfuxml.exception.OfxAuthoringException;
 import org.openfuxml.factory.xml.ofx.content.XmlCommentFactory;
-import org.openfuxml.factory.xml.ofx.content.structure.XmlParagraphFactory;
 import org.openfuxml.factory.xml.ofx.content.structure.XmlSectionFactory;
-import org.openfuxml.factory.xml.ofx.content.text.XmlTitleFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class OfxQaCategorySectionFactory extends AbstractUtilsOfxDocumentationFactory
+public class OfxQaCategoriesSectionFactory extends AbstractUtilsOfxDocumentationFactory
 {
-	final static Logger logger = LoggerFactory.getLogger(OfxQaCategorySectionFactory.class);
+	final static Logger logger = LoggerFactory.getLogger(OfxQaCategoriesSectionFactory.class);
 
-	public OfxQaCategorySectionFactory(Configuration config, String lang, Translations translations)
+	public OfxQaCategoriesSectionFactory(Configuration config, String lang, Translations translations)
 	{
 		super(config,lang,translations);
 	}
 	
-	public Section build(Category category) throws OfxAuthoringException
+	public Section build(Qa qa, String path) throws OfxAuthoringException
 	{
 		Section section = XmlSectionFactory.build();
-
-		section.getContent().add(XmlTitleFactory.build(category.getName()));
+		section.setContainer(true);
 		
 		Comment comment = XmlCommentFactory.build();
 		DocumentationCommentBuilder.doNotModify(comment);
 		section.getContent().add(comment);
 		
-		Paragraph p = XmlParagraphFactory.build();
-		p.getContent().add("Test");
-		section.getContent().add(p);
+		for(Category c : qa.getCategory())
+		{
+			section.getContent().add(buildCategory(c,path));
+		}
 		
+		return section;
+	}
+	
+	private Section buildCategory(Category c, String path)
+	{
+		Section section = XmlSectionFactory.build();
+		section.setContainer(true);
+		section.setInclude(path+"/"+c.getCode());
 		return section;
 	}
 	
