46bbc8d9ec9bc2c8adb161888258b01ef784156c
thorsten-k <t.kisner@web.de>
2014-06-19 05:52:57 +0000

    latex packages moved to ofx

--- doc/src/main/java/net/sf/ahtutils/doc/latex/UtilsLatexDefinitionBuilder.java
+++ doc/src/main/java/net/sf/ahtutils/doc/latex/UtilsLatexDefinitionBuilder.java
@@ -1,56 +1,23 @@
 package net.sf.ahtutils.doc.latex;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-
 import net.sf.ahtutils.doc.UtilsDocumentation;
-import net.sf.ahtutils.exception.processing.UtilsConfigurationException;
-import net.sf.exlp.util.io.FileIO;
-import net.sf.exlp.util.io.resourceloader.MultiResourceLoader;
 
 import org.apache.commons.configuration.Configuration;
-import org.apache.commons.io.IOUtils;
+import org.openfuxml.exception.OfxConfigurationException;
+import org.openfuxml.renderer.latex.util.OfxLatexDefinitionBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class UtilsLatexDefinitionBuilder
+public class UtilsLatexDefinitionBuilder extends OfxLatexDefinitionBuilder
 {	
 	final static Logger logger = LoggerFactory.getLogger(UtilsLatexDefinitionBuilder.class);
 		
-	private MultiResourceLoader mrl;
-	private File baseLatex;
+	public static enum Code {colors}
 	
 	public UtilsLatexDefinitionBuilder(Configuration config)
 	{
-		mrl = new MultiResourceLoader();
-		baseLatex = new File(config.getString(UtilsDocumentation.keyBaseDocDir));
+		super(config.getString(UtilsDocumentation.keyBaseDocDir));
 	}
 	
-	public void copyPackages() throws UtilsConfigurationException
-	{
-		try
-		{
-			File fTarget = new File(baseLatex,"tex"+File.separator+"packages.tex");
-			InputStream is = mrl.searchIs("tex.aht-utils/packages.tex");
-			byte[] bytes = IOUtils.toByteArray(is);
-			FileIO.writeFileIfDiffers(bytes, fTarget);
-		}
-		catch (FileNotFoundException e) {throw new UtilsConfigurationException(e.getMessage());}
-		catch (IOException e) {throw new UtilsConfigurationException(e.getMessage());}
-	}
-	
-	public void copyColors() throws UtilsConfigurationException
-	{
-		try
-		{
-			File fTarget = new File(baseLatex,"tex"+File.separator+"colors.tex");
-			InputStream is = mrl.searchIs("tex.aht-utils/colors.tex");
-			byte[] bytes = IOUtils.toByteArray(is);
-			FileIO.writeFileIfDiffers(bytes, fTarget);
-		}
-		catch (FileNotFoundException e) {throw new UtilsConfigurationException(e.getMessage());}
-		catch (IOException e) {throw new UtilsConfigurationException(e.getMessage());}
-	}
+	public void copyColors() throws OfxConfigurationException {copyResource("tex.aht-utils","colors");}
 }
