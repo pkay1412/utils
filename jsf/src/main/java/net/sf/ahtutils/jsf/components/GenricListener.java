8157729cc6554b19b2848949c98d8fa6a2dc818d
thorsten-k <t.kisner@web.de>
2014-12-02 07:28:19 +0000

    GenericListener added

---
+++ jsf/src/main/java/net/sf/ahtutils/jsf/components/GenricListener.java
@@ -0,0 +1,17 @@
+package net.sf.ahtutils.jsf.components;
+
+import javax.el.MethodExpression;
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UINamingContainer;
+import javax.faces.context.FacesContext;
+
+@FacesComponent(value="net.sf.ahtutils.jsf.components.GenericListener")
+public class GenricListener extends UINamingContainer
+{
+    public void listener()
+    {
+        FacesContext context = FacesContext.getCurrentInstance();
+        MethodExpression ajaxEventListener = (MethodExpression) getAttributes().get("listener");
+        ajaxEventListener.invoke(context.getELContext(), new Object[] {});
+    }
+}
