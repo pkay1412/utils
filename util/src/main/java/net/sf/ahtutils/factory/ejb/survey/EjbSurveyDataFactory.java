e342c5da7f8afc905ca9c9995eefa7f9ae1e182e
thorsten-k <t.kisner@web.de>
2015-02-06 10:35:05 +0000

    EjbSurveyDataFactory added

---
+++ util/src/main/java/net/sf/ahtutils/factory/ejb/survey/EjbSurveyDataFactory.java
@@ -0,0 +1,62 @@
+package net.sf.ahtutils.factory.ejb.survey;
+
+import net.sf.ahtutils.interfaces.model.survey.UtilsSurvey;
+import net.sf.ahtutils.interfaces.model.survey.UtilsSurveyAnswer;
+import net.sf.ahtutils.interfaces.model.survey.UtilsSurveyCorrelation;
+import net.sf.ahtutils.interfaces.model.survey.UtilsSurveyData;
+import net.sf.ahtutils.interfaces.model.survey.UtilsSurveyOption;
+import net.sf.ahtutils.interfaces.model.survey.UtilsSurveyQuestion;
+import net.sf.ahtutils.interfaces.model.survey.UtilsSurveySection;
+import net.sf.ahtutils.interfaces.model.survey.UtilsSurveyTemplate;
+import net.sf.ahtutils.model.interfaces.status.UtilsDescription;
+import net.sf.ahtutils.model.interfaces.status.UtilsLang;
+import net.sf.ahtutils.model.interfaces.status.UtilsStatus;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EjbSurveyDataFactory<L extends UtilsLang,
+										D extends UtilsDescription,
+										SURVEY extends UtilsSurvey<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,
+										SS extends UtilsStatus<SS,L,D>,
+										TEMPLATE extends UtilsSurveyTemplate<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,
+										TS extends UtilsStatus<TS,L,D>,
+										TC extends UtilsStatus<TC,L,D>,
+										SECTION extends UtilsSurveySection<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,
+										QUESTION extends UtilsSurveyQuestion<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,
+										UNIT extends UtilsStatus<UNIT,L,D>,
+										ANSWER extends UtilsSurveyAnswer<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,
+										DATA extends UtilsSurveyData<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,
+										OPTION extends UtilsSurveyOption<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,
+										CORRELATION extends UtilsSurveyCorrelation<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>>
+{
+	final static Logger logger = LoggerFactory.getLogger(EjbSurveyDataFactory.class);
+	
+	final Class<DATA> cData;
+    
+	public EjbSurveyDataFactory(final Class<DATA> cData)
+	{       
+        this.cData = cData;
+	}
+	
+	public static <L extends UtilsLang,D extends UtilsDescription,SURVEY extends UtilsSurvey<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,SS extends UtilsStatus<SS,L,D>,TEMPLATE extends UtilsSurveyTemplate<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,TS extends UtilsStatus<TS,L,D>,TC extends UtilsStatus<TC,L,D>,SECTION extends UtilsSurveySection<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,QUESTION extends UtilsSurveyQuestion<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,UNIT extends UtilsStatus<UNIT,L,D>,ANSWER extends UtilsSurveyAnswer<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,DATA extends UtilsSurveyData<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,OPTION extends UtilsSurveyOption<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>,CORRELATION extends UtilsSurveyCorrelation<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>>
+	EjbSurveyDataFactory<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION> factory(final Class<DATA> cData)
+	{
+		return new EjbSurveyDataFactory<L,D,SURVEY,SS,TEMPLATE,TS,TC,SECTION,QUESTION,UNIT,ANSWER,DATA,OPTION,CORRELATION>(cData);
+	}
+	
+	public DATA build(SURVEY survey, CORRELATION correlation)
+	{
+		DATA ejb = null;
+		try
+		{
+			ejb = cData.newInstance();
+			ejb.setSurvey(survey);
+			ejb.setCorrelation(correlation);
+		}
+		catch (InstantiationException e) {e.printStackTrace();}
+		catch (IllegalAccessException e) {e.printStackTrace();}
+		
+		return ejb;
+	}
+}
