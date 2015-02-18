8840c8f67746f948ee30a94e113ba7720c3955dc
hhemmer <hhemmer@web.de>
2015-02-18 06:46:35 +0000

    Fixed ReturnNullStrategy

--- util/src/main/java/net/sf/ahtutils/db/excel/strategies/ReturnNullStrategy.java
+++ util/src/main/java/net/sf/ahtutils/db/excel/strategies/ReturnNullStrategy.java
@@ -1,18 +1,22 @@
 package net.sf.ahtutils.db.excel.strategies;
 
+import java.util.Hashtable;
+
 import net.sf.ahtutils.db.excel.ImportStrategy;
 import net.sf.ahtutils.interfaces.facade.UtilsFacade;
 
 public class ReturnNullStrategy implements ImportStrategy {
 
-	@Override
 	public Object handleObject(Object object, String parameterClass) {
 		return null;
 	}
 
-	@Override
 	public void setFacade(UtilsFacade facade) {
 		// Not needed here
 	}
+	
+	private Hashtable<String, Object> tempPropertyStore;
+	public Hashtable<String, Object> getTempPropertyStore() {return tempPropertyStore;}
+	public void setTempPropertyStore(Hashtable<String, Object> tempPropertyStore) {this.tempPropertyStore = tempPropertyStore;}
 
 }
