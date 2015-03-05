649b9470c93ae183ef0ed2b9bdb63c2f388808bf
thorsten-k <t.kisner@web.de>
2015-03-05 10:15:05 +0000

    documentation updated

--- doc/src/main/resources/listing.aht-utils/admin/db/postgres/template/debian/wheezy.sql
+++ doc/src/main/resources/listing.aht-utils/admin/db/postgres/template/debian/wheezy.sql
@@ -1,6 +1,6 @@
 createdb template_postgis
 createlang plpgsql template_postgis
 psql -d template_postgis -c "UPDATE pg_database SET datistemplate=true WHERE datname='template_postgis'"
-psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-2.0/postgis.sql
-psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-2.0/spatial_ref_sys.sql
-psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-2.0/postgis_comments.sql
+psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
+psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
+psql -d template_postgis -f /usr/share/postgresql/9.1/contrib/postgis_comments.sql
--- doc/src/main/resources/listing.aht-utils/admin/db/postgres/tuning/debian/wheezy.sql
+++ doc/src/main/resources/listing.aht-utils/admin/db/postgres/tuning/debian/wheezy.sql
@@ -1,1 +1,3 @@
-TBD
+psql -U postgres -d @@@APP@@@ -c "ALTER TABLE geography_columns OWNER TO @@@APP@@@;"
+psql -U postgres -d @@@APP@@@ -c "ALTER TABLE geometry_columns OWNER TO @@@APP@@@;"
+psql -U postgres -d @@@APP@@@ -c "ALTER TABLE spatial_ref_sys OWNER TO @@@APP@@@;"
