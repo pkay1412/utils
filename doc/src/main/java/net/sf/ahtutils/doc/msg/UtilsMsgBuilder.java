b88a4519fb8bc6f11857e6c8491e27307a424e63
thorsten-k <t.kisner@web.de>
2015-03-06 09:58:51 +0000

    generic msg definition added

--- doc/src/main/java/net/sf/ahtutils/doc/msg/UtilsMsgBuilder.java
+++ doc/src/main/java/net/sf/ahtutils/doc/msg/UtilsMsgBuilder.java
@@ -23,6 +23,7 @@ import org.slf4j.LoggerFactory;
 
 public class UtilsMsgBuilder
 {	
+	public static final String srcGeneric = "msg.aht-utils/generic.xml";
 	public static final String srcAdminSystemProperty = "msg.aht-utils/admin/system/properties.xml";
 	public static final String srcAdminSystemDbStatistic = "msg.aht-utils/admin/system/dbStatistic.xml";
 	public static final String srcAdminSystemDbDump = "msg.aht-utils/admin/system/dbDump.xml";
