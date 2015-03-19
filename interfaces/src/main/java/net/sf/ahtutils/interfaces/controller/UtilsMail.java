731ecbd09b031f7451105ffa78f02c1da0eb8d2a
thorsten-k <t.kisner@web.de>
2015-03-19 13:31:17 +0000

    type in key fixed

--- interfaces/src/main/java/net/sf/ahtutils/interfaces/controller/UtilsMail.java
+++ interfaces/src/main/java/net/sf/ahtutils/interfaces/controller/UtilsMail.java
@@ -1,13 +1,12 @@
 package net.sf.ahtutils.interfaces.controller;
 
-
 public interface UtilsMail
 {		
 	public final String smtpActive = "net.smtp.active";
 	public final String smtpHost = "net.smtp.host";
 	
 	public final String smtpUser = "net.smtp.user";
-	public final String smtpPwd = "net.smtp.host";
+	public final String smtpPwd = "net.smtp.pwd";
 	
 	public final String smtpOverride = "net.smtp.override";
 	public final String smtpOverrideTo = "net.smtp.override.to";
