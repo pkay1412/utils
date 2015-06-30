f860fef5051418ec4a185a2a4149bda55a02a133
thorsten-k <t.kisner@web.de>
2015-06-30 12:21:07 +0000

    support for labels added for XmlCategoryFactory

--- util/src/main/java/net/sf/ahtutils/factory/xml/status/XmlCategoryFactory.java
+++ util/src/main/java/net/sf/ahtutils/factory/xml/status/XmlCategoryFactory.java
@@ -1,21 +1,24 @@
 package net.sf.ahtutils.factory.xml.status;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
 import net.sf.ahtutils.xml.status.Category;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class XmlCategoryFactory
 {
 	final static Logger logger = LoggerFactory.getLogger(XmlCategoryFactory.class);
-		
+			
+	private String lang;
 	private Category q;
 	
-	public XmlCategoryFactory(Category q)
+	public XmlCategoryFactory(Category q){this(null,q);}
+	public XmlCategoryFactory(String lang,Category q)
 	{
+		this.lang=lang;
 		this.q=q;
 	}
 	
@@ -37,6 +40,26 @@ public class XmlCategoryFactory
 
 		}
 		
+		if(q.isSetLabel() && lang!=null)
+		{
+			if(ejb.getName()!=null)
+			{
+				if(ejb.getName().containsKey(lang)){xml.setLabel(ejb.getName().get(lang).getLang());}
+				else
+				{
+					String msg = "No translation "+lang+" available in "+ejb;
+					logger.warn(msg);
+					xml.setLabel(msg);
+				}
+			}
+			else
+			{
+				String msg = "No @name available in "+ejb;
+				logger.warn(msg);
+				xml.setLabel(msg);
+			}
+		}
+		
 		return xml;
 	}
 	
@@ -44,6 +67,14 @@ public class XmlCategoryFactory
 	{
 		Category xml = new Category();
 		xml.setCode(code);
+		return xml;
+	}
+	
+	public static Category label(String code, String label)
+	{
+		Category xml = new Category();
+		xml.setCode(code);
+		xml.setLabel(label);
 		return xml;
 	}
 }
