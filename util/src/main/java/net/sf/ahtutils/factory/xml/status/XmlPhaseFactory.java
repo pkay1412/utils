c59e974781053b19f6a5748075351798c0f0e49c
thorsten-k <t.kisner@web.de>
2015-07-02 12:41:53 +0000

    label support added

--- util/src/main/java/net/sf/ahtutils/factory/xml/status/XmlPhaseFactory.java
+++ util/src/main/java/net/sf/ahtutils/factory/xml/status/XmlPhaseFactory.java
@@ -1,21 +1,23 @@
 package net.sf.ahtutils.factory.xml.status;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.sf.ahtutils.interfaces.model.status.UtilsDescription;
 import net.sf.ahtutils.interfaces.model.status.UtilsLang;
 import net.sf.ahtutils.interfaces.model.status.UtilsStatus;
 import net.sf.ahtutils.xml.status.Phase;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class XmlPhaseFactory
 {
 	final static Logger logger = LoggerFactory.getLogger(XmlPhaseFactory.class);
 		
+	private String lang;
 	private Phase q;
 	
-	public XmlPhaseFactory(Phase q)
+	public XmlPhaseFactory(String lang,Phase q)
 	{
+		this.lang=lang;
 		this.q=q;
 	}
 	
@@ -37,6 +39,26 @@ public class XmlPhaseFactory
 
 		}
 		
+		if(q.isSetLabel() && lang!=null)
+		{
+			if(ejb.getName()!=null)
+			{
+				if(ejb.getName().containsKey(lang)){xml.setLabel(ejb.getName().get(lang).getLang());}
+				else
+				{
+					String msg = "No translation "+lang+" available in "+ejb;
+					logger.warn(msg);
+					xml.setLabel(msg);
+				}
+			}
+			else
+			{
+				String msg = "No @name available in "+ejb;
+				logger.warn(msg);
+				xml.setLabel(msg);
+			}
+		}
+		
 		return xml;
 	}
 	
@@ -44,6 +66,14 @@ public class XmlPhaseFactory
 	{
 		Phase xml = new Phase();
 		xml.setCode(code);
+		return xml;
+	}
+	
+	public static Phase buildLabel(String code, String label)
+	{
+		Phase xml = new Phase();
+		xml.setCode(code);
+		xml.setLabel(label);
 		return xml;
 	}
 }
