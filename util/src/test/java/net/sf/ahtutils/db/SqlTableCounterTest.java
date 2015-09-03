66ec23dd4acfcb64e637370ea710e30f91846e29
RebeccaRoblick <rebecca.roblick@gmail.com>
2015-09-03 08:00:47 +0200

    relative file path in SqlTableCounterTest edited

--- util/src/test/java/net/sf/ahtutils/db/SqlTableCounterTest.java
+++ util/src/test/java/net/sf/ahtutils/db/SqlTableCounterTest.java
@@ -7,7 +7,7 @@ public class SqlTableCounterTest {
     public static void main(String[] args) {
 
         //relative Pfadangabe?
-        SqlTableCounter sqltc = new SqlTableCounter("C:\\Users\\roblick\\WorkspaceIntelliJ\\utils\\util\\src\\test\\resources\\db","db1.txt", "db2.txt");
+        SqlTableCounter sqltc = new SqlTableCounter("util\\src\\test\\resources\\db","db1.txt", "db2.txt");
         sqltc.output();
     }
 }
