3e63158e75fddf1f66305dba43b26ae8c3dbf885
Thorsten <t.kisner@web.de>
2015-09-03 08:24:19 +0200

    using Java6

--- util/src/main/java/net/sf/ahtutils/db/sql/SqlTableCounter.java
+++ util/src/main/java/net/sf/ahtutils/db/sql/SqlTableCounter.java
@@ -1,61 +1,65 @@
 package net.sf.ahtutils.db.sql;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import org.openfuxml.content.table.Table;
-import org.openfuxml.exception.OfxAuthoringException;
 import org.openfuxml.factory.xml.table.OfxTableFactory;
 import org.openfuxml.renderer.text.OfxTextRenderer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
+public class SqlTableCounter
+{
+	private Map<String, String> dbmap1 = new LinkedHashMap<String, String>();
+	private Map<String, String> dbmap2 = new LinkedHashMap<String, String>();
+	
+	private File f1;
+	private File f2;
 
-public class SqlTableCounter {
+	public SqlTableCounter(String path, String filename1, String filename2)
+	{
+		f1 = new File(path, filename1);
+		f2 = new File(path, filename2);
+	}
+	
+	public void readData(File f, Map<String,String> map) throws IOException
+	{
+		LineIterator it = FileUtils.lineIterator(f, "UTF-8");
+		try
+		{
+		   while (it.hasNext())
+		   {
+		     String line = it.nextLine();
+		     String[] s = line.split("\\|");
+             map.put(s[0], s[1]);
+		   }
+		}
+		finally {it.close();}
+	}
 
-        private Map<String, String> dbmap1 = new LinkedHashMap<String, String>();
-        private Map<String, String> dbmap2 = new LinkedHashMap<String, String>();
-        private Path filepath1;
-        private Path filepath2;
-
-        public SqlTableCounter(String stringpath1, String filename1, String filename2) {
-            filepath1 = FileSystems.getDefault().getPath(stringpath1, filename1);
-            filepath2 = FileSystems.getDefault().getPath(stringpath1, filename2);
-        }
-
-        void readData(){
-            try {
-                BufferedReader br1 = Files.newBufferedReader(filepath1, Charset.forName("UTF-8"));
-                BufferedReader br2 = Files.newBufferedReader(filepath2, Charset.forName("UTF-8"));
-                String input1;
-                String input2 = null;
-                while((input1 = br1.readLine()) != null || (input2 = br2.readLine()) != null){
-                    if(input1 != null) {
-                        String[] line = input1.split("\\|");
-                        dbmap1.put(line[0], line[1]);
-                    }
-                    if(input2 != null){
-                        String[] line = input2.split("\\|");
-                        dbmap2.put(line[0], line[1]);
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+	public List<Object[]> data() throws IOException
+	{
+		readData(f1,dbmap1);
+		readData(f2,dbmap2);
+		
+		List<Object[]> result = new ArrayList<Object[]>();
+		Map<String, String> done = new HashMap<String, String>();
+		
+		for(Map.Entry<String, String> me : dbmap1.entrySet())
+		{
+			if(!dbmap2.containsKey(me.getKey()))
+            {
+                Object[] tmp = {me.getKey(), "X", "", me.getValue(), "", ""};
+                result.add(tmp);
             }
-        }
-
-        public List<Object[]> data() {
-            readData();
-            List<Object[]> result = new ArrayList<Object[]>();
-            Map<String, String> done = new HashMap<String, String>();
-            for(Map.Entry<String, String> me : dbmap1.entrySet()){
-                if(!dbmap2.containsKey(me.getKey())) {
-                    Object[] tmp = {me.getKey(), "X", "", me.getValue(), "", ""};
-                    result.add(tmp);
-                }
-                for (Map.Entry<String, String> me2 : dbmap2.entrySet()) {
+			for (Map.Entry<String, String> me2 : dbmap2.entrySet())
+			{
                     if(me.getKey().equals(me2.getKey())) {
                         if(me.getValue().equals(me2.getValue())) {
                             Object[] tmp = {me.getKey(), "X", "X", me.getValue(), me2.getValue(),"X"};
@@ -76,25 +80,21 @@ public class SqlTableCounter {
             return result;
         }
 
-        List<String> header(){
-            List<String> header =  new ArrayList<String>();
-            header.add("table");
-            header.add("db1");
-            header.add("db2");
-            header.add("c1");
-            header.add("c2");
-            header.add("equal");
-            return header;
-        }
+	private List<String> header()
+	{
+		List<String> header =  new ArrayList<String>();
+		header.add("table");
+		header.add("db1");
+		header.add("db2");
+		header.add("c1");
+		header.add("c2");
+		header.add("equal");
+		return header;
+	}
 
-        public void output(){
-            Table t = OfxTableFactory.build(header(), data());
-            try {
-                OfxTextRenderer.table(t, System.out);
-            } catch (OfxAuthoringException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+	public void output() throws Exception
+	{
+		Table t = OfxTableFactory.build(header(), data());
+		OfxTextRenderer.table(t, System.out);
+	}
 }
