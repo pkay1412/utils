dca672ed18726f60edfd3c4d18abfa0999e79d7d
RebeccaRoblick <rebecca.roblick@gmail.com>
2015-09-03 11:22:53 +0200

    sort() added

--- util/src/main/java/net/sf/ahtutils/db/sql/SqlTableCounter.java
+++ util/src/main/java/net/sf/ahtutils/db/sql/SqlTableCounter.java
@@ -2,11 +2,7 @@ package net.sf.ahtutils.db.sql;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
@@ -28,57 +24,63 @@ public class SqlTableCounter
 		f2 = new File(path, filename2);
 	}
 	
-	public void readData(File f, Map<String,String> map) throws IOException
-	{
-		LineIterator it = FileUtils.lineIterator(f, "UTF-8");
-		try
-		{
-		   while (it.hasNext())
-		   {
-		     String line = it.nextLine();
-		     String[] s = line.split("\\|");
-             map.put(s[0], s[1]);
-		   }
-		}
-		finally {it.close();}
-	}
+    public void readData(File f, Map<String,String> map) throws IOException
+    {
+        LineIterator it = FileUtils.lineIterator(f, "UTF-8");
+        try
+        {
+            while (it.hasNext())
+            {
+                String line = it.nextLine();
+                String[] s = line.split("\\|");
+                map.put(s[0], s[1]);
+
+            }
+        }
+        finally {it.close();}
+    }
 
 	public List<Object[]> data() throws IOException
 	{
 		readData(f1,dbmap1);
-		readData(f2,dbmap2);
-		
-		List<Object[]> result = new ArrayList<Object[]>();
+		readData(f2, dbmap2);
+
 		Map<String, String> done = new HashMap<String, String>();
 		
 		for(Map.Entry<String, String> me : dbmap1.entrySet())
 		{
 			if(!dbmap2.containsKey(me.getKey()))
             {
-                Object[] tmp = {me.getKey(), "X", "", me.getValue(), "", ""};
-                result.add(tmp);
+                done.put(me.getKey(), me.getKey() + "|X| |" + me.getValue() + "| | ") ;
             }
 			for (Map.Entry<String, String> me2 : dbmap2.entrySet())
 			{
-                    if(me.getKey().equals(me2.getKey())) {
-                        if(me.getValue().equals(me2.getValue())) {
-                            Object[] tmp = {me.getKey(), "X", "X", me.getValue(), me2.getValue(),"X"};
-                            result.add(tmp);
-                        }
-                        else {
-                            Object[] tmp = {me.getKey(), "X", "X", me.getValue(), me2.getValue(), ""};
-                            result.add(tmp);
-                        }
+                if(me.getKey().equals(me2.getKey()))
+                {
+                    if(me.getValue().equals(me2.getValue()))
+                    {
+                        done.put(me.getKey(), me.getKey() + "|X|X|" + me.getValue() + "|" + me2.getValue() + "|X") ;
                     }
-                    else if (!dbmap1.containsKey(me2.getKey()) && !done.containsKey(me2.getKey()) ) {
-                        Object[] tmp = {me2.getKey(), "", "X", "", me2.getValue(), ""};
-                        result.add(tmp);
-                        done.put(me2.getKey(), me2.getValue()) ;
+                    else
+                    {
+                        done.put(me.getKey(), me.getKey() + "|X|X|" + me.getValue()+ "|" + me2.getValue() + "| ") ;
                     }
                 }
+                else if (!dbmap1.containsKey(me2.getKey()) && !done.containsKey(me2.getKey()) )
+                {
+                    done.put(me2.getKey(), me2.getKey() + "| |X| |" + me2.getValue() + "| ") ;
+                }
             }
-            return result;
         }
+        done = sort(done);
+        List<Object[]> result = new ArrayList<Object[]>();
+        for(Map.Entry<String,String> me : done.entrySet())
+        {
+            Object tmp [] = me.getValue().split("\\|");
+            result.add(tmp);
+        }
+        return result;
+    }
 
 	private List<String> header()
 	{
@@ -97,4 +99,25 @@ public class SqlTableCounter
 		Table t = OfxTableFactory.build(header(), data());
 		OfxTextRenderer.table(t, System.out);
 	}
+
+    public Map<String, String> sort(Map<String, String> data)
+    {
+        List<Map.Entry<String,String>> unsort = new ArrayList<Map.Entry<String, String>>(data.entrySet());
+        Collections.sort(unsort, new Comparator<Map.Entry<String, String>>()
+        {
+            @Override
+            public int compare(Map.Entry<String, String> a, Map.Entry<String, String> b)
+            {
+                return a.getKey().compareTo(b.getKey());
+            }
+        });
+        Map<String, String> sort = new LinkedHashMap<String, String>();
+        for (Map.Entry<String, String> entry : unsort)
+        {
+            sort.put(entry.getKey(), entry.getValue());
+        }
+        return sort;
+    }
+
+
 }
--- util/src/test/java/net/sf/ahtutils/db/SqlTableCounterTest.java
+++ util/src/test/java/net/sf/ahtutils/db/SqlTableCounterTest.java
@@ -7,7 +7,7 @@ public class SqlTableCounterTest
     public static void main(String[] args) throws Exception
     {
 
-        SqlTableCounter sqltc = new SqlTableCounter("../util/src/test/resources/db","db1.txt", "db2.txt");
+        SqlTableCounter sqltc = new SqlTableCounter("util/src/test/resources/db","db1.txt", "db2.txt");
         sqltc.output();
     }
 }
