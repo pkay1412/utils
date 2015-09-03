a07d5be5d7baea3f2a214ac4a654d6d1fa7d5404
Thorsten <t.kisner@web.de>
2015-09-03 08:26:40 +0200

    java6 classes

--- util/src/test/java/net/sf/ahtutils/db/SqlTableCounterTest.java
+++ util/src/test/java/net/sf/ahtutils/db/SqlTableCounterTest.java
@@ -2,9 +2,10 @@ package net.sf.ahtutils.db;
 
 import net.sf.ahtutils.db.sql.SqlTableCounter;
 
-
-public class SqlTableCounterTest {
-    public static void main(String[] args) {
+public class SqlTableCounterTest
+{
+    public static void main(String[] args) throws Exception
+    {
 
         SqlTableCounter sqltc = new SqlTableCounter("../util/src/test/resources/db","db1.txt", "db2.txt");
         sqltc.output();
