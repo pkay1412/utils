8e5d044e482ede8a1bb4f942660f57475ac7422e
RebeccaRoblick <rebecca.roblick@gmail.com>
2015-09-29 17:01:40 +0200

    overload notNull Method

--- util/src/main/java/net/sf/ahtutils/controller/constraints/ConstraintsChecker.java
+++ util/src/main/java/net/sf/ahtutils/controller/constraints/ConstraintsChecker.java
@@ -1,6 +1,9 @@
 package net.sf.ahtutils.controller.constraints;
 
 import net.sf.ahtutils.util.reflection.ReflectionsUtil;
+import net.sf.ahtutils.xml.system.Constraint;
+import net.sf.ahtutils.xml.system.ConstraintAttribute;
+import net.sf.ahtutils.xml.system.ConstraintScope;
 
 public class ConstraintsChecker {
     public static boolean notNull(Object object, String attribute) throws Exception
@@ -10,5 +13,31 @@ public class ConstraintsChecker {
                                                     object.getClass(),
                                                     object)
                                                     != null;
+	}
+    
+    public static String notNull(Object object, String attribute, ConstraintScope scope) throws Exception
+    {
+    	String methode = ("get" + attribute.substring(0, 1).toUpperCase() + attribute.substring(1));
+    	if(ReflectionsUtil.simpleInvokeMethod(methode,
+                                                  new Object[]{},
+                                                  object.getClass(),
+                                                  object)
+                                                  == null)
+    	{
+	    	for (Constraint con : scope.getConstraint())
+	        {
+	            if(con.getType().getCode().equals("notNull"))
+	            {
+	            	for(ConstraintAttribute a : con.getConstraintAttribute())
+	            	{
+	            		if(a.getAttribute().equals(attribute))
+	            		{
+	            			return con.getDescriptions().getDescription().get(0).getValue();
+	            		}
+	            	}
+	            }
+	        }
+    	}
+        return null;
 	}
 }
